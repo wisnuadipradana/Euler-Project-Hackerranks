0793e2b40a95c3623d0778c4bac841f3992b28c5
uzumaki nagato tenshou <49567907+wisnuadipradana@users.noreply.github.com>
2021-01-20 19:03:43 +0700

    Update Project Euler Factorial digit sum.py

--- Euler Project/Project Euler Factorial digit sum.py
+++ Euler Project/Project Euler Factorial digit sum.py
@@ -11,17 +11,3 @@ t = int(input().strip())
 for a0 in range(t):
     n = int(input().strip())
     print(sum([int(i) for i in str(math.factorial(n))]))
-'''
-
-def faktorial(x):
-    if x == 1:
-        return 1
-    else :
-        return x*faktorial(x-1)
-
-t = int(input().strip())
-for a0 in range(t):
-    n = int(input().strip())
-    print(sum([int(i) for i in str(faktorial(n))]))
-
-'''
